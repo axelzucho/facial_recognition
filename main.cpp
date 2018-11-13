965a9440c6496ce975dad58d53001902ed9265e8
Rodrigo <rodrigogm971@gmail.com>
2018-11-13 15:09:02 -0600

    Interface restart

--- main.cpp
+++ main.cpp
@@ -43,146 +43,157 @@ int main()
 	//Create database object
 	cv::Mat template_image;
 	dlib::full_object_detection shape;
-
-	cv::VideoCapture video(0);
-	video.open(0);
-
-	while (!video.isOpened()) {
-	    std::cout << "Error al intentar abrir la camara" << std::endl;
-	}
-
-	std::cout << "Ingrese el número de la operación deseada:\n1. Reconocer una persona en camara\n2. Verificar si la persona ubicada coincide con la matricula ingresada\n3. Enrolar una persona nueva" << std::endl;
-	bool flag = true;
-	while (flag)
+	
+	bool run_program = true;
+	while(run_program)
 	{
-		cv::Mat frame;
-		video >> frame;
-		all_faces = face_recognition.face_detector_->detect_faces(&frame);
-		real_faces = face_recognition.face_detector_->ignore_false_positives(&frame, all_faces, 2);
-		largest_face[0] = face_recognition.face_detector_->get_largest_face(real_faces);
-		Mat frame_with_rectangles = frame.clone();
-		face_recognition.face_detector_->show_faces(&frame_with_rectangles, all_faces, real_faces, largest_face[0]);
-		face_recognition.face_aligner_->Detect(frame, largest_face[0], shape);
-		char key_pressed = cv::waitKey(1);
-		switch(key_pressed)
-		{
-			case '1':
-			std::cout << "1";
-			cv::destroyAllWindows();
-        	video.release();
-        	std::cout << "Ingrese la matricula" << "\n";
-				// Aqui se implementa caso 1
-				{
-					string matricula;
-					std::cin >> matricula;
-					std::pair<int, BiographicalData> result_case_1;
-					result_case_1 = face_recognition.caso1(&frame, shape, matricula);
-					
-					if(result_case_1.first == 1){
-						std::cout << "La persona concuerda con la matrícula ingresada\n";
-						cv::Mat recognized_image;
-						recognized_image = cv::imread(result_case_1.second.img, cv::IMREAD_COLOR);
-						
-						/*cv::resize(frame, frame, cv::Size(150, 150), 0, 0, cv::INTER_CUBIC);
-						cv::hconcat(frame, recognized_image, recognized_image);
-						cv::imshow("Recognized image vs Database Image",  recognized_image);
-						cv::waitKey(0);*/
-					    
-					}
-					else {
-						std::cout << "La persona NO concuerda con la matrícula ingresada\n";
-					}
-				}
-        	flag = false;
-			break;
+		cv::VideoCapture video(0);
+		video.open(0);
 
-			case '2':
-			std::cout << "2";
-			cv::destroyAllWindows();
-        	video.release();
-        	// Aqui se implementa caso 2
-				{
-					if(!frame.empty())
-					{
-						std::pair<int, BiographicalData> result_case_2;
-						result_case_2 = face_recognition.caso2(&frame, shape);
-
-						std::cout << "Regresó información de la función en el main" << std::endl;
-						
-						if(result_case_2.first == 1){
-							cv::Mat recognized_image;
-	    					recognized_image = cv::imread(result_case_2.second.img, cv::IMREAD_COLOR);
-	    					cv::resize(frame, frame, cv::Size(150, 150), 0, 0, cv::INTER_CUBIC);
-	    					cv::hconcat(frame, recognized_image, recognized_image);
-	    					cv::imshow( "Recognized image vs Database Image",  recognized_image);
-	    					cv::waitKey(0);
-							std::cout << "La persona fue reconocida en la base de datos como: " << result_case_2.second.name << " " << result_case_2.second.lastName << " con la matrícula: " << result_case_2.second.matricula << "\n";
-						}
-						else{
-							std::cout << "La persona no fue reconocida\n";
-						}
-					}else{
-						flag = true;
-						break;
-					}
-				}
-        	flag = false;
-			break;
-
-			case '3':
-			std::cout << "1";
-			cv::destroyAllWindows();
-        	video.release();
-        	std:: cout << "Ingrese los datos a escribir" << "\n";
-        	// Aqui se implementa caso 3
-				{
-					string matricula, name, last_name, mail;
-					int age,result_case_3=31;
-					while(result_case_3!=1)
-					{
-					if(result_case_3&1)
-					{
-					std:: cout << "Ingrese la matricula" << "\n";
-					std::cin >> matricula;
-					}
-					if(result_case_3&2)
-					{
-					std:: cout << "Ingrese el nombre" << "\n";
-					std::cin >> name;
-					}
-					if(result_case_3&4)
-					{
-					std:: cout << "Ingrese el apellido" << "\n";
-					std::cin >> last_name;
-					}
-					if(result_case_3&8)
-					{
-					std:: cout << "Ingrese el mail" << "\n";
-					std::cin >> mail;
-					}
-					if(result_case_3&16)
-					{
-					std:: cout << "Ingrese la edad" << "\n";
-					std::cin >> age;
-					}
-					BiographicalData bio;
-					bio.matricula = matricula;
-					bio.age = age;
-					bio.name = name;
-					bio.lastName = last_name;
-					bio.mail = mail;
-					result_case_3 = face_recognition.enroll(frame, shape, bio);
-					std::cout << result_case_3<<std::endl;
-					}
-					std::cout << "La persona fue registrada\n";
-				}
-        	flag = false;
-			break;
-
-			default:
-				break;
+		while (!video.isOpened()) {
+		    std::cout << "Error al intentar abrir la camara" << std::endl;
 		}
 
+		std::cout << "Ingrese el número de la operación deseada:\n1. Reconocer una persona en camara\n2. Verificar si la persona ubicada coincide con la matricula ingresada\n3. Enrolar una persona nueva" << std::endl;
+		bool flag = true;
+		while (flag)
+		{
+			cv::Mat frame;
+			video >> frame;
+			all_faces = face_recognition.face_detector_->detect_faces(&frame);
+			real_faces = face_recognition.face_detector_->ignore_false_positives(&frame, all_faces, 2);
+			largest_face[0] = face_recognition.face_detector_->get_largest_face(real_faces);
+			Mat frame_with_rectangles = frame.clone();
+			face_recognition.face_detector_->show_faces(&frame_with_rectangles, all_faces, real_faces, largest_face[0]);
+			face_recognition.face_aligner_->Detect(frame, largest_face[0], shape);
+			char key_pressed = cv::waitKey(1);
+			switch(key_pressed)
+			{
+				case '1':
+				std::cout << "1";
+				cv::destroyAllWindows();
+	        	video.release();
+	        	std::cout << "Ingrese la matricula" << "\n";
+					// Aqui se implementa caso 1
+					{
+						string matricula;
+						std::cin >> matricula;
+						std::pair<int, BiographicalData> result_case_1;
+						result_case_1 = face_recognition.caso1(&frame, shape, matricula);
+						
+						if(result_case_1.first == 1){
+							std::cout << "La persona concuerda con la matrícula ingresada\n";
+							cv::Mat recognized_image;
+							recognized_image = cv::imread(result_case_1.second.img, cv::IMREAD_COLOR);
+							
+							/*cv::resize(frame, frame, cv::Size(150, 150), 0, 0, cv::INTER_CUBIC);
+							cv::hconcat(frame, recognized_image, recognized_image);
+							cv::imshow("Recognized image vs Database Image",  recognized_image);
+							cv::waitKey(0);*/
+						    
+						}
+						else {
+							std::cout << "La persona NO concuerda con la matrícula ingresada\n";
+						}
+					}
+	        	flag = false;
+				break;
+
+				case '2':
+				std::cout << "2";
+				cv::destroyAllWindows();
+	        	video.release();
+	        	// Aqui se implementa caso 2
+					
+				if(!frame.empty())
+				{
+					std::pair<int, BiographicalData> result_case_2;
+					result_case_2 = face_recognition.caso2(&frame, shape);
+
+					std::cout << "Regresó información de la función en el main" << std::endl;
+					
+					if(result_case_2.first == 1){
+						cv::Mat recognized_image;
+    					recognized_image = cv::imread(result_case_2.second.img, cv::IMREAD_COLOR);
+    					cv::resize(frame, frame, cv::Size(150, 150), 0, 0, cv::INTER_CUBIC);
+    					cv::hconcat(frame, recognized_image, recognized_image);
+    					cv::imshow( "Recognized image vs Database Image",  recognized_image);
+    					cv::waitKey(0);
+						std::cout << "La persona fue reconocida en la base de datos como: " << result_case_2.second.name << " " << result_case_2.second.lastName << " con la matrícula: " << result_case_2.second.matricula << "\n";
+					}
+					else{
+						std::cout << "La persona no fue reconocida\n";
+					}
+				}else{
+					flag = true;
+					break;
+				}
+					
+	        	flag = false;
+				break;
+
+				case '3':
+				std::cout << "1";
+				cv::destroyAllWindows();
+	        	video.release();
+	        	std:: cout << "Ingrese los datos a escribir" << "\n";
+	        	// Aqui se implementa caso 3
+					{
+						string matricula, name, last_name, mail;
+						int age,result_case_3=31;
+						while(result_case_3!=1)
+						{
+						if(result_case_3&1)
+						{
+						std:: cout << "Ingrese la matricula" << "\n";
+						std::cin >> matricula;
+						}
+						if(result_case_3&2)
+						{
+						std:: cout << "Ingrese el nombre" << "\n";
+						std::cin >> name;
+						}
+						if(result_case_3&4)
+						{
+						std:: cout << "Ingrese el apellido" << "\n";
+						std::cin >> last_name;
+						}
+						if(result_case_3&8)
+						{
+						std:: cout << "Ingrese el mail" << "\n";
+						std::cin >> mail;
+						}
+						if(result_case_3&16)
+						{
+						std:: cout << "Ingrese la edad" << "\n";
+						std::cin >> age;
+						}
+						BiographicalData bio;
+						bio.matricula = matricula;
+						bio.age = age;
+						bio.name = name;
+						bio.lastName = last_name;
+						bio.mail = mail;
+						result_case_3 = face_recognition.enroll(frame, shape, bio);
+						std::cout << result_case_3<<std::endl;
+						}
+						std::cout << "La persona fue registrada\n";
+					}
+	        	flag = false;
+				break;
+
+				case '0':
+					cv::destroyAllWindows();
+					video.release();
+					run_program = false;
+					flag = false;
+					break;
+
+				default:
+					break;
+			}
+
+		}
 	}
 
 	return 0;
